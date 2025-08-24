d48b5b5b4272fc050e3af6b520a74a3d09036da6
manneyfest <anneymwaniki@gmail.com>
2025-08-24 13:41:00 +0300

    Fix:Corrected index.css file for a full-width layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 import Navbar from './Layout/Navbar.jsx';
-import Hero from './Components/Hero.jsx'; // Import the Hero component
+import Hero from './Components/Hero.jsx';
 
 const App = () => {
   return (
     <div className="bg-gray-900 min-h-screen">
       
       <Navbar />
-      <main className="text-white p-4">
-        <Hero /> {/* Render the Hero component here */}
+      <Hero />
+      <div className="text-white p-4">
         <h1 className="text-3xl font-bold mb-4">Welcome to your Anime Database!</h1>
         <p>Your anime search results and details will appear here.</p>
-      </main>
+      </div>
     </div>
   );
 };
