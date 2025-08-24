9bcb56703203f4cda9bcf777d89e1b9a4837242c
manneyfest <anneymwaniki@gmail.com>
2025-08-24 13:10:34 +0300

    Added a background image to Hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section>
+    <section className="relative h-[500px] bg-cover bg-center rounded-b-lg shadow-lg"
+      style={{ backgroundImage: `url('https://i.imgur.com/G5g208B.jpg')` }}>
       {/* Hero content will go here */}
     </section>
   );
